34a9e87b871c46627c4cf023a0a7620b0f7f92cf
Anthonius Munthi <toni.munthi@gmail.com>
2012-12-21 14:14:22 +0800

    add git feature to toolbox

---
+++ src/toolbox/git/command/BaseCommand.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the toolbox package.
+ *
+ * (c) Anthonius Munthi <me@itstoni.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace toolbox\git\command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Description of BaseCommand
+ *
+ * @author Anthonius Munthi <toni.munthi@gmail.com>
+ */
+abstract class BaseCommand extends Command
+{
+    protected function runGit()
+    {
+
+    }
+    
+    protected function initialize(InputInterface $input, OutputInterface $output)
+    {
+
+    }
+}
+
+?>
