2ab121273a61ba30055fdb22bc425d97bd1ef2b1
Anthonius Munthi <toni.munthi@gmail.com>
2012-12-24 03:51:03 +0800

    some fixes

--- src/toolbox/core/test/ToolboxTestCase.php
+++ src/toolbox/core/test/ToolboxTestCase.php
@@ -11,32 +11,5 @@ use Symfony\Component\Console\Output\StreamOutput;
 
 class ToolboxTestCase extends TestCase
 {
-	static protected $application;    
-    
-    static public function getApplication()
-    {
-    	if(!is_object(static::$application)){
-    		$app = new TestApplication();
-    		$app->setAutoExit(false);
-    		static::$application = $app;
-    	}
-    	return self::$application;
-    }
-    
-    public function runCommand($command)
-    {
-    	$application = static::getApplication();
-    	$fp = tmpfile();
-    	$input = new StringInput($command);
-    	$output = new StreamOutput($fp);
-    	 
-    	$application->run($input, $output);
-    	 
-    	fseek($fp, 0);    	
-    	while (!feof($fp)) {
-    		$output = fread($fp, 4096);
-    	}
-    	fclose($fp);    	    	    
-    	return "\n".$output."\n";
-    }
+
 }
